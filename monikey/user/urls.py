1db9ed98e7972f83f75dd5e8a6f0722a8befe545
Edson Ney Martins Júnior <edsonneyjr@gmail.com>
2023-05-06 13:50:38 -0300

    pagamentos e planos atualizados

--- monikey/user/urls.py
+++ monikey/user/urls.py
@@ -7,9 +7,9 @@ router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('login', login_user, name="login_user"),
-    path('logout', logout_user, name="logout_user"),
-    path('register', register_user, name="register_user"),
-    path('change_password', change_password, name='change_password'),
+    path('login/', login_user, name="login_user"),
+    path('logout/', logout_user, name="logout_user"),
+    path('register/', register_user, name="register_user"),
+    path('change_password/', change_password, name='change_password'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\mggrl\Documents\GitHub\MONIKEY-PlanSystem\venv
+set VIRTUAL_ENV=C:\Users\ultim\OneDrive\Documentos\GitHub\MONIKEY-PlanSystem\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
