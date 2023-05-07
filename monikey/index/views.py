a0e9351cb39687fb25d00a5d03f4007edbf6258b
Edson Ney Martins Júnior <edsonneyjr@gmail.com>
2023-05-07 09:56:14 -0300

    story user

--- monikey/index/views.py
+++ monikey/index/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.http import JsonResponse
-import nfc
 import threading
 
 # Create your views here.
